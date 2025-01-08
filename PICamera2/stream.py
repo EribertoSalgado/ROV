3eb5f316062e3558e5ea05817685f6dd42e9424e
Eriberto Salgado <salgadoe@sonoma.edu>
2025-01-07 18:44:43 -0800

    Update stream.py

--- PICamera2/stream.py
+++ PICamera2/stream.py
@@ -1,6 +1,7 @@
 # flask might be able to be removed.
 # Livestream the CSI camera on a network for several local devices to stream. It is slow, but it
 # may be best to use a local IP address to minimize delay. 
+# http://10.0.0.116/streamattempt.php the file is located in /var/www/html
 from flask import Flask, Response
 from picamera2 import Picamera2
 import cv2
