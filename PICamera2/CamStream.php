908e1de4914d796966c1934da7c4394222d32552
Eriberto Salgado <salgadoe@sonoma.edu>
2025-01-07 18:46:25 -0800

    Create CamStream.php

---
+++ PICamera2/CamStream.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ROV Video Feed</title>
+</head>
+<body>
+    <h1>ROV Video Stream</h1>
+    <!-- Embed the Flask video feed in an iframe -->
+    <iframe src="http://10.0.0.116:5000/video_feed" width="640" height="480" frameborder="0" allowfullscreen></iframe>
+</body>
+</html>
