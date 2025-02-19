29914d55368a8df461644fbb97c1f0bb26641ab2
Eriberto Salgado <salgadoe@sonoma.edu>
2025-02-19 13:09:33 -0800

    Update CamStream.py

--- PICamera2/CamStream.py
+++ PICamera2/CamStream.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
-# flask might be able to be removed.
-# Livestream the CSI camera on a network for several local devices to stream. It is slow, but it
-# may be best to use a local IP address to minimize delay. 
-# http://10.0.0.116/CamDash.php the file is located in /var/www/html
-# http://10.0.0.116/CamDash.php The file is located in /var/www/html
-# http://10.0.0.116:5000/video_feed
-# run using: python3 <fileName>
-
-from flask import Flask, Response
+# Two separate flasks required to stream and snapshot
+from flask import Flask, Response, send_file
 from picamera2 import Picamera2
 import cv2
+import io
 
 app = Flask(__name__)
 
@@ -19,8 +13,6 @@ camera.configure(camera.create_preview_configuration(main={"format": 'XRGB8888',
 camera.start()
 
 # Further zoom-out by defining a larger ROI
-# The coordinates are (x, y, width, height)
-# Adjusting width and height to zoom out more
 roi = (0, 0, 1920, 1080)  # Increase the width and height to zoom out further
 camera.set_controls({"ScalerCrop": roi})
 
@@ -35,6 +27,19 @@ def generate_frames():
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/snapshot')
+def snapshot():
+    # Capture a snapshot
+    frame = camera.capture_array()
+    ret, buffer = cv2.imencode('.jpg', frame)
+    img_bytes = buffer.tobytes()
+
+    # Convert the byte data to a file-like object
+    img_io = io.BytesIO(img_bytes)
+    img_io.seek(0)
+
+    return send_file(img_io, mimetype='image/jpeg')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
